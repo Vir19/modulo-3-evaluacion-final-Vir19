923b01eca17f60abb4695bbed37d33e6bd26c4e1
Vir19 <viir19@hotmail.com>
2024-02-18 21:05:02 +0100

    insert placeholder picture

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -2,10 +2,17 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 function CharacterCard({ character }) {
+  const placeHolderPicture =
+    "https://mir-s3-cdn-cf.behance.net/projects/404/dfaf93111214253.Y3JvcCwyMjgwLDE3ODMsMCw1MA.jpg";
+
   return (
     <Link className="link" to={`/character/${character.id}`}>
       <div className="characterCard" key={character.id}>
-        <img className="personPic" src={character.image} alt={character.name} />
+        <img
+          className="personPic"
+          src={character.image || placeHolderPicture}
+          alt={character.name}
+        />
         <h3 className="personName">{character.name}</h3>
         <p className="personRace">{character.species}</p>
       </div>
